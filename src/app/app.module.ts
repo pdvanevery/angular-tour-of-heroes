ffd73960b9b073850ee0fab5a34c99c027d69ffb
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-10-31 16:52:10 -0700

    changed names now save to remote server until refresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,16 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   imports: [
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }),
     BrowserModule,
     FormsModule,
     AppRoutingModule
