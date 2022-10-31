183eb8f113e25c563e5f738497c0dca4a2de76c6
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-10-31 12:56:13 -0700

    added working routes for each individual hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,21 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  //returns a single value, the array of mock heroes
-  getHeroes():Observable<Hero[]> {
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  //injects the MessageService into the HeroService
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
